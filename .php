2fde4ca15a8c1086bf07a94a9210b9884a36c73a
Ariel Caminos <caminosariel@gmail.com>
2019-12-13 22:41:17 -0300

    Create .php

---
+++ .php
@@ -0,0 +1,24 @@
+<h1> Solicitud de consulta </h1>
+
+<form action="BaseDeDatos.php" method="post">
+
+<br>Nombre: <input type="text"  name="Nombre"><br>
+    
+<br>Apellido: <input type="text"    name="Apellido"><br>
+    
+<br>Tipo de consulta: <select name="TipoConsulta"><br>
+    
+    <option value="Personal">Personal</option>
+
+    <option value="Empresarial">Empresarial</option>
+
+    <option value="Otros">Otros</option>
+    
+    </select><br>
+
+<br>Correo electronico: <input type="text"  name="E-mail"><br>
+    
+<br>Ingrese su consulta: 
+<textarea name="Consulta">    </textarea><br>
+
+<br>  <input type="submit"    value="enviar">
