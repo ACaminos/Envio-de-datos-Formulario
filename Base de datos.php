f97a3f5f46a70cb57fed25b5e1c44bcfb4deb5ef
Ariel Caminos <caminosariel@gmail.com>
2019-12-13 22:42:00 -0300

    Create Base de datos.php

---
+++ Base de datos.php
@@ -0,0 +1,9 @@
+<h1> Solicitud de contacto </h1>
+
+El cliente <?php echo $_POST["Nombre"]?> <?php echo $_POST["Apellido"]?> ha solicitado el tipo de consulta <?php echo $_POST["TipoConsulta"]?> 
+<br>
+<br>La consulta es la siguiente:<br>
+<?php echo $_POST["Consulta"]?>
+<br><br>
+<br>Favor de comunicarse el mail que dejo a continuación:
+<?php echo $_POST["E-mail"]?>
