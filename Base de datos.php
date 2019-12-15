5ed19f656c3eb4179d0db1264bbbbc3ae57e59a9
Ariel Caminos <caminosariel@gmail.com>
2019-12-15 12:18:14 -0300

    Update Base de datos.php

--- Base de datos.php
+++ Base de datos.php
@@ -1,9 +1,14 @@
 <h1> Solicitud de contacto </h1>
 
+<?php
+$Consulta= $_POST["Consulta"];
+$NuevaConsulta= filter_var($Consulta, FILTER_SANITIZE_STRING);
+?>
+
 El cliente <?php echo $_POST["Nombre"]?> <?php echo $_POST["Apellido"]?> ha solicitado el tipo de consulta: <?php echo $_POST["TipoConsulta"]?> 
 <br>
 <br>La consulta es la siguiente:<br>
-<?php echo $_POST["Consulta"]?>
+<br> <?php echo $NuevaConsulta?>
 <br><br>
 <br>Favor de comunicarse al mail que dejo a continuación:
 <?php echo $_POST["E-mail"]?>
